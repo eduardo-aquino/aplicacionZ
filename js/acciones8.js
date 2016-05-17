a50f7b3e76491e5140779c510e49a98ce6d24a56
eduardo-aquino <eduardoaquino4g.dam@gmail.com>
2016-05-17 12:10:43 -0500

    4subida

--- js/acciones8.js
+++ js/acciones8.js
@@ -1,25 +1,26 @@
 // JavaScript Document
 
- $(document).ready(function (e) {
-	document.addEventListener ("deviceready",function(){
+$(document).ready(function(e) {
+    document.addEventListener("deviceready",function(){
 		
-	$('#izquierda').on("swipeleft",function(){
-	navigator.notification.alert("Deslizo a Ia izquierda",function(){"ApIicacion8","Aceptar"});
-	});
-$('#derecha').on("swiperight",function(){
-	navigator.notification.confirm ("¿Que quieres hacer?",function(opt){
-		
-	switch(opt)
-	{
-	case 1:
-	navigator.notification.beep(1);
-	break;
+		$('#izquierda').on("swipeleft",function(){
+			navigator.notification. alert("Deslizo a la izquierda",function(){"Aplicacion8","Aceptar"});
+});
 
-	case 2:
-	navigator.notification.vibrate(1000);
-	break;
-	}
+$('#derecha').on("swiperight",function(){
+	navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+		switch(opt)
+		{
+			case 1:
+			navigator.notification.beep(1);
+			break;
+			
+			case 2:
+			navigator.notification.vibrate(1000);
+			break;
+		}
 	},"Aplicacion8","Beep,Vibrar,Cancelar");
+	
 });
 	},false);
-})
+});
